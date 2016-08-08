c3ea502b5182b7bcb06fd7e8b1505206c5e0141c
Abhijit Gadgil <abhijit.gadgil@izeltech.com>
2016-08-08 08:02:49 +0530

    Started with #8 - making sure can run `python setup.py nosetests`

---
+++ package/tests/test_openstack_shell.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+
+from mock import Mock
+
+from cloudshell.cp.openstack.openstack_shell import OpenStackShell
+
+
+class TestOpenStackShell(TestCase):
+
+    def setUp(self):
+        pass
+
+
+    def test_power_on(self):
+        self.assertTrue(True)
+
+    def test_power_off(self):
+        self.assertTrue(True)
+
+    def test_deploy_instance(self):
+        self.assertTrue(True)
+
+    def test_delete_instance(self):
+        self.assertTrue(True)
+
+    def test_refresh_ip(self):
+        self.assertTrue(True)
+
+
