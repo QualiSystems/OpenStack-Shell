a9dd0f0c69f05d32eeedc5681b60eb0ae0d64619
Abhijit Gadgil <abhijit.gadgil@izeltech.com>
2016-08-10 22:36:10 +0530

    #7 - Started using the new 'contexts'
    Using `With Context` syntax for the top level driver
    Updated requirements to require higher version of cloudshell-shell-core

--- drivers/deployment_drivers/openstack_nova_image_instance/driver.py
+++ drivers/deployment_drivers/openstack_nova_image_instance/driver.py
@@ -1,10 +1,9 @@
 import jsonpickle
 
-# The ResourceDriverInterface
+# ResourceDriverInterface and other Context utilities from shell.core
 from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
-
-# Logger
-from cloudshell.core.logger.qs_logger import get_qs_logger
+from cloudshell.shell.core.session.cloudshell_session import CloudShellSessionContext
+from cloudshell.shell.core.session.logging_session import LoggingSessionContext
 
 # From OpenStack package
 from cloudshell.cp.openstack.common.driver_helper import CloudshellDriverHelper
@@ -12,59 +11,48 @@ from cloudshell.cp.openstack.common.deploy_data_holder import DeployDataHolder
 from cloudshell.cp.openstack.models.deploy_os_nova_image_instance_resource_model \
                             import DeployOSNovaImageInstanceResourceModel
 
-class DeployOSNovaImageInstanceDriver(ResourceDriverInterface):
 
-    CAT_NAME = "OpenStackCloudShell"
-    CLASS_NAME = 'DeployOSNovaImageInstanceDriver'
+class DeployOSNovaImageInstanceDriver(ResourceDriverInterface):
     APP_NAME = 'app_name'
-    IMAGE_PARAMS = 'img_params'
+    IMAGE_PARAM = 'image'
 
     def __init__(self):
-        # FIXME: Right now there's CloudShellDriverHelper is there in
-        #        each of 'cloud-shells' a better place is 'cloudshell.core'?
-        self.cs_helper = CloudshellDriverHelper()
-        self.logger = None
+        pass
 
     def cleanup(self):
         pass
 
     def initialize(self, context):
-        #reservation_id = context.reservation.reservation_id
-        log_group = self.CLASS_NAME
-	log_cat = self.CAT_NAME
-
-        self.logger = get_qs_logger(log_category=log_cat,
-                                    log_group=log_group)
+        pass
 
     def Deploy(self, context, Name=None):
         """
         :param context: reservation context
         :type context:
+        :param Name:
+        :type Name: str
         """
+        with LoggingSessionContext(context) as logger:
+            with CloudShellSessionContext(context) as session:
+                logger.debug("Deploy Called for Reservation: {0}".format(
+                                        context.reservation.reservation_id))
+                # Get CS Session we are going to make an API call using this session
+                logger.debug("creating session: {0}, {1}, {2}".format(
+                                context.connectivity.server_address,
+                                context.connectivity.admin_auth_token,
+                                context.reservation.domain))
 
-        self.logger.debug("Deploy Called for Reservation : %(res_id)s:" %
-                            {'res_id' : context.reservation.reservation_id})
-        # Get CS Session we are going to make an API call using this session
-        self.logger.debug("creating sessionx: %(server)s, %(token)s, %(domain)s"
-                            % { 'server': context.connectivity.server_address,
-                                'token': context.connectivity.admin_auth_token,
-                                'domain': context.reservation.domain})
-        session = self.cs_helper.get_session(context.connectivity.server_address,
-                                        context.connectivity.admin_auth_token,
-                                        context.reservation.domain)
+                deploy_service_res_model = DeployOSNovaImageInstanceResourceModel()
 
-        deploy_service_res_model = DeployOSNovaImageInstanceResourceModel()
+                app_name = jsonpickle.decode(context.resource.app_context.app_request_json)['name']
 
-        app_name = jsonpickle.decode(
-                        context.resource.app_context.app_request_json)['name']
+                deploy_req = DeployDataHolder({self.APP_NAME: app_name,
+                                               self.IMAGE_PARAM: deploy_service_res_model})
 
-        deploy_req = DeployDataHolder({self.APP_NAME: app_name,
-                                       self.IMAGE_PARAMS : deploy_service_res_model})
+                logger.debug("Calling the Shell Driver's Deploy method for app: "
+                             " {0}".format(app_name))
 
-        self.logger.debug("Calling the Shell Driver's Deploy method for app: " \
-                            " %(name)s" % {'name':app_name})
-
-        return str(jsonpickle.encode({"vm_name": "testvm", "vm_uuid": "1234-5678",
-                                    "cloud_provider_resource_name" : "openstack"},
-                                    unpicklable=False))
+                return str(jsonpickle.encode({"vm_name": "testvm", "vm_uuid": "1234-5678",
+                                              "cloud_provider_resource_name": "openstack"},
+                                             unpicklable=False))
 
