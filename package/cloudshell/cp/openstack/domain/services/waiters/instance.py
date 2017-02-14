867b63436a7ada916af8341b5ff23942aadb6dc9
Alex Azarh <Alex.az@qualisystems.com>
2017-02-14 15:35:27 +0200

    added tests

--- package/cloudshell/cp/openstack/domain/services/waiters/instance.py
+++ package/cloudshell/cp/openstack/domain/services/waiters/instance.py
@@ -23,10 +23,9 @@ class InstanceWaiter(object):
     def __init__(self, cancellation_service, delay=10):
         """
         :param int delay: Time in seconds between each poll
-        :param int timeout: Timeout after which Exception will be raised
         """
         self.cancellation_service = cancellation_service
-        self.delay = delay
+        self._delay = delay
 
     def wait(self, instance, state, cancellation_context, logger):
         """
@@ -45,7 +44,7 @@ class InstanceWaiter(object):
             instance.get()
             if cancellation_context is not None:
                 self.cancellation_service.check_if_cancelled(cancellation_context=cancellation_context)
-            time.sleep(self.delay)
+            time.sleep(self._delay)
 
         if instance.status == self.ERROR:
             raise InstanceErrorStateException(format(instance.fault['message']))
