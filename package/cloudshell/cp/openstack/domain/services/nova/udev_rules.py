c1818f659e672b949784a28d0d303ada5e77f93e
Abhijit Gadgil <abhijit.gadgil@izeltech.com>
2017-02-27 18:44:18 +0530

    #159 reopen - fix
     Now added rules that work for Ubuntu and CentOS. Also work if Python 2 is
     not installed by default. Verified on Ubuntu 16.04 and  CentOS 7.2
     Cloud images.

--- package/cloudshell/cp/openstack/domain/services/nova/udev_rules.py
+++ package/cloudshell/cp/openstack/domain/services/nova/udev_rules.py
@@ -1,35 +1,44 @@
 
-udev_rules_str = \
-'''#!/usr/bin/env python
+udev_rules_sh_str = '''#!/bin/sh
 
-import sys
-import os
+PYTHON=
+if [ -e '/usr/bin/python' ]; then
+    PYTHON=/usr/bin/python
+elif [ -e '/usr/bin/python3' ]; then
+    PYTHON=/usr/bin/python3
+fi
 
-NETDEV_RULES_FILE = "/etc/udev/rules.d/70-attach-interface-qs-cloudshell.rules"
+OS_TYPE=`$PYTHON -c 'import sys;print(sys.platform)'`
 
-if sys.platform.lower().find("linux") < 0:
-    print "Non Linux OS. Doing Nothing..."
-    sys.exit(0)
+LOGGER_TAG="ADD_QS_TAG"
+
+if [ $OS_TYPE != 'linux2' -a $OS_TYPE != 'linux' ]; then
+    logger -i -s --tag ${LOGGER_TAG} "Platform $OS_TYPE is not supported for Udev rules."
+    exit 1
+fi
+
+QS_UDEV_FILE='/etc/udev/rules.d/70-a-qs-connectivity.rules'
+
+logger -i -s --tag ${LOGGER_TAG}  "Creating UDEV Entries in file $QS_UDEV_FILE"
+cat <<TOEND > $QS_UDEV_FILE
+# Udev Rules for Quali Systems Apply Connectivity
+
+# KERNEL=="eth0", GOTO="no_eth0_config"
+
+SUBSYSTEM=="net", ACTION=="add", KERNEL=="eth*", RUN+="/sbin/ifconfig %k up", RUN+="/sbin/dhclient -v %k -lf /var/run/dhclient-qs-%k.lease -pf /var/run/dhclient-qs-%k.pid"'
+SUBSYSTEM=="net", ACTION=="add", KERNEL=="en*", RUN+="/sbin/ifconfig %k up", RUN+="/sbin/dhclient -v %k -lf /var/run/dhclient-qs-%k.lease -pf /var/run/dhclient-qs-%k.pid"'
+
+# LABEL="no_eth0_config"
+
+TOEND
+
+# We are going to stop renaming of the devices - this messes things up. Everything will be eth0->ethN
+logger -i -s --tag $LOGGER_TAG "Disabling device name change."
+/bin/ln -s /dev/null /etc/udev/rules.d/80-net-name-slot.rules
+/bin/ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
 
 
-def generate_udev_rules_str():
-    if not os.path.exists("/sbin/ifconfig"):
-        print "Ifconfig does not exist. Doing nothing..."
-        sys.exit(0)
+logger -i -s --tag $LOGGER_TAG  "Calling udevadm control"
+/sbin/udevadm control --reload
 
-    if not os.path.exists("/sbin/dhclient"):
-        print "dhclient does not exist or not in standard path. Doing Noting."
-        sys.exit(0)
-
-    etc_net_file_str = '\\n'.join(['KERNEL=="eth0", GOTO="no_eth0_config"', 'SUBSYSTEM=="net", ACTION=="add", KERNEL=="eth*", RUN+="/sbin/ifconfig %k up", RUN+="/sbin/dhclient -v %k -lf /var/run/dhclient--%.lease -pf /var/run/dhclient--%k.pid"', '', 'LABEL="no_eth0_config"', ''])
-
-    print etc_net_file_str
-    with open(NETDEV_RULES_FILE, "w") as f:
-        f.write(etc_net_file_str)
-
-    result = os.system("udevadm control --reload")
-
-    print result
-
-generate_udev_rules_str()
 '''
